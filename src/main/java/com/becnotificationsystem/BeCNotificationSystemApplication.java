35844e22cf727b343fa678d0f4532c00126f28cd
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2026-05-01 01:11:24 +0900

    Initial commit

---
+++ src/main/java/com/becnotificationsystem/BeCNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.becnotificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCNotificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeCNotificationSystemApplication.class, args);
+    }
+
+}
