0bce7623452fb6038f64541a49ad8a5617d019c5
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2026-05-03 02:31:31 +0900

    feat: AsyncConfig 추가

---
+++ src/main/java/com/becnotificationsystem/global/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.becnotificationsystem.global.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("notification-async-");
+        executor.initialize();
+        return executor;
+    }
+}
